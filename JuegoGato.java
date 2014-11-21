50223bcd2e1dd0cc41f62d767ca1ebff566cd433
ultimus96meromero12 <jd_medina96@hotmail.com>
2014-11-21 07:17:42 -0600

    cordenada y

--- JuegoGato.java
+++ JuegoGato.java
@@ -2,15 +2,23 @@ import java.util.Scanner;
 public class JuegoGato{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String[][] = new String[3][3];
+		char[][] table= new Char[3][3];
 		boolean win = false, turno=true;
 		int resp;
 
 		do{
 			resp = scan.nextInt();
 			switch(resp){
-				case 1: 
+				case 1:
 
+
+
+
+				do{
+					System.out.println("Ingrese columna:");
+					y= lea.nextInt();
+
+				}while(y<0|| y > table.length);
 				break;
 			}
 		}while(rest!=2);
