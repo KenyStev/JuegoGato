9ba9702331dbc92bea2c671e4061b5f2351d2ce1
KenyStev <kjestevez.0795@gmail.com>
2014-11-21 07:31:09 -0600

    dowm

--- JuegoGato.java
+++ JuegoGato.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 public class JuegoGato{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+<<<<<<< HEAD
+		char[][] table= new Char[3][3];
+=======
 		char[][] table= new char[3][3];
+>>>>>>> 01b98ae75399ce61b0f373a2434c4476663a6afe
 		boolean win = false, turno=true;
 		int resp, x,y;
 
@@ -14,19 +18,27 @@ public class JuegoGato{
 			resp = scan.nextInt();
 			switch(resp){
 				case 1: 
-					do{
-						for(int f=0; f<table.length; f++){
-							for (int c=0; c<table[f].length; c++) {
-								
-							}
-						}
 
+					for(int f=0; f<table.length; f++){
+						for (int c=0; c<table[f].length; c++) {	
+							System.out.print(table[f][c] + " ");
+						}
+						System.out.println();
+					}
+
+					do{
 						System.out.print("Ingrese fila: ");
 						x = scan.nextInt();
 						if(x<0 || x>table.length){
 							System.out.println("Fila Incorrecta!");
 						}
 					}while(x<0 || x>table.length);
+
+					do{
+						System.out.println("Ingrese columna:");
+						y= scan.nextInt();
+
+					}while(y<0|| y > table.length);
 				break;
 			}
 		}while(rest!=2);
