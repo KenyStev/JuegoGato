1e32af045e3dac3ac61f627e2b357601fa0d6c28
KenyStev <kjestevez.0795@gmail.com>
2014-11-21 07:03:03 -0600

    Menu

--- JuegoGato.java
+++ JuegoGato.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 public class JuegoGato{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		String[][] = new String[3][3];
+		boolean win = false, turno=true;
+		int resp;
+
+		do{
+			resp = scan.nextInt();
+			switch(resp){
+				case 1: 
+
+				break;
+			}
+		}while(rest!=2);
 	}
 }
